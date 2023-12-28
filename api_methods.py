5fe99e3f263925ceabcd73377f54e186e66af08f
nurfkutlu <fatmanurkutlu1@gmail.com>
2023-12-28 23:47:02 +0300

    a method added

--- api_methods.py
+++ api_methods.py
@@ -23,7 +23,9 @@ def get_channel_stats(youtube, channel_ids):
         all_data.append(data)
     
     return pd.DataFrame(all_data)
-
+"""
+---------------------------------------------------------
+"""
 # method 2
 
 def get_video_ids(youtube, playlist_id):
@@ -57,3 +59,66 @@ def get_video_ids(youtube, playlist_id):
 
         
     return video_ids
+
+"""
+---------------------------------------------------------
+"""
+#Method 3 
+
+def get_most_pop_Youtube_TR(youtube):
+    all_data = []
+
+# This request method is defined as below . You can include what you want in part. 
+    # https://developers.google.com/youtube/v3/docs/channels/list#usage you can look detailly.
+    
+    request = youtube.videos().list(
+        part="snippet,contentDetails,statistics",
+        chart="mostPopular",
+        regionCode="TR",
+    )
+    response = request.execute()
+
+
+    # loop through items in response
+    #json webpage ( https://jsonformatter.org/json-editor) is sooo good to anayze.
+    for item in response["items"]:
+        data = {'channelName': item['snippet']['title'],
+                'PublishTime': item['snippet']['publishedAt'],
+                'views': item['statistics']['viewCount'],
+                'LikeCount':item['statistics']['likeCount'],
+                'CommentCount':item['statistics']['commentCount'],
+                'Duration': item['contentDetails']['duration'],
+                'videoID': item['id']
+        }
+        all_data.append(data)
+
+        """ 
+        ------------------------------------------------------
+        
+        """
+
+# Get comments for the specified video
+comments = []
+nextPageToken = None
+
+while True:
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId="5nXRi1dkRps",
+        maxResults=500,  # Adjust as needed
+        pageToken=nextPageToken
+    )
+    response = request.execute()
+
+    for item in response["items"]:
+        comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+        comments.append(comment)
+
+    nextPageToken = response.get("nextPageToken")
+
+    if not nextPageToken:
+        break
+
+# Print or use the retrieved comments
+for comment in comments:
+    print(comment)
